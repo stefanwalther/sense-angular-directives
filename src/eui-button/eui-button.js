cf005e9a6529fca7d1460fd436473ac3999f4ef3
Stefan Walther <swr@selun-swr-mac.local>
2017-05-05 12:56:12 +0200

    chore: Add linting

--- src/eui-button/eui-button.js
+++ src/eui-button/eui-button.js
@@ -1,28 +1,28 @@
-/*global define*/
-define( [
-	'qvangular',
-	'./eui-button.ng.html'
-], function ( qvangular,
-			  ngTemplate ) {
-	'use strict';
+/* global define */
+define([
+  'qvangular',
+  './eui-button.ng.html'
+], function (qvangular,
+             ngTemplate) {
+  'use strict';
 
-	var component = {
-		restrict: 'E',
-		replace: true,
-		template: ngTemplate,
-		scope: {
-			close: '&',
-			closable: '=',
-			visible: '='
-		},
-		controller: ['$scope', '$attrs', function ( scope, attrs ) {
-		}]
-	};
+  var component = {
+    restrict: 'E',
+    replace: true,
+    template: ngTemplate,
+    scope: {
+      close: '&',
+      closable: '=',
+      visible: '='
+    },
+    controller: ['$scope', '$attrs', function (/* scope, attrs */) {
+    }]
+  };
 
-	qvangular.directive( "euiButton", function () {
-		return component;
-	} );
+  qvangular.directive('euiButton', function () {
+    return component;
+  });
 
-	return component;
+  return component;
 
-} );
+});
--- src/eui-version-check/eui-version-check.js
+++ src/eui-version-check/eui-version-check.js
@@ -1,28 +1,28 @@
-/*global define*/
-define( [
-	'qvangular',
-	'!text./eui-version-check.ng.html'
-], function ( qvangular,
-			  ngTemplate ) {
-	'use strict';
+/* global define */
+define([
+  'qvangular',
+  '!text./eui-version-check.ng.html'
+], function (qvangular,
+             ngTemplate) {
+  'use strict';
 
-	var component = {
-		restrict: 'A',
-		replace: false,
-		template: ngTemplate,
-		scope: true,
-		controller: ['$scope', function ( $scope ) {
-			$scope.showWarning = false;
-		}],
-		link: function ( scope, elem, attrs ) {
+  var component = {
+    restrict: 'A',
+    replace: false,
+    template: ngTemplate,
+    scope: true,
+    controller: ['$scope', function ($scope) {
+      $scope.showWarning = false;
+    }],
+    link: function (/* scope, elem  , attrs */) {
 
-		}
-	};
+    }
+  };
 
-	qvangular.directive( "euiVersionCheck", function () {
-		return component;
-	} );
+  qvangular.directive('euiVersionCheck', function () {
+    return component;
+  });
 
-	return component;
+  return component;
 
-} );
+});
