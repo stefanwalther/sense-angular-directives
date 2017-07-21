37acc1442a2dc370cd7b36ba9bc3e766f027da41
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-07-21 02:51:54 +0200

    Bump v0.4.3

--- dist/eui-tablesort/eui-tablesort.js
+++ dist/eui-tablesort/eui-tablesort.js
@@ -2,7 +2,7 @@
 
 * sense-angular-directives - AngularJS directives ready to be used in Qlik Sense visualization extensions.
 * --
-* @version v0.4.1
+* @version v0.4.2
 * @link https://github.com/stefanwalther/sense-angular-directives
 * @author Stefan Walther
 * @license MIT
